a02bca36bb8a07ddac4945b6387f1fc9b7b191c2
Jeffery Anyimadu <jefferyasamoahbless@gmail.com>
2026-02-06 15:11:23 +0000

    testdata: add parameterized test data for customer scenarios

---
+++ src/test/java/com/bank/tests/testdata/CustomerTestData.java
@@ -0,0 +1,23 @@
+package com.bank.tests.testdata;
+
+import org.junit.jupiter.params.provider.Arguments;
+import java.util.stream.Stream;
+
+
+public class CustomerTestData {
+
+    public static Stream<Arguments> depositAmounts() {
+        return Stream.of(
+                Arguments.of("100"),
+                Arguments.of("500")
+        );
+    }
+
+    public static Stream<Arguments> invalidWithdrawals() {
+        return Stream.of(
+                Arguments.of("-50"),
+                Arguments.of("100000")
+        );
+    }
+}
+
