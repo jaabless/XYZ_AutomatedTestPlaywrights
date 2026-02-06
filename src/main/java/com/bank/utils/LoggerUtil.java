25aba085b806663cdcb19444847d10663b33463c
Jeffery Anyimadu <jefferyasamoahbless@gmail.com>
2026-02-06 15:04:46 +0000

    core: add logging utilities

---
+++ src/main/java/com/bank/utils/LoggerUtil.java
@@ -0,0 +1,14 @@
+package com.bank.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggerUtil {
+
+    private LoggerUtil() {}
+
+    public static Logger getLogger(Class<?> clazz) {
+        return LoggerFactory.getLogger(clazz);
+    }
+}
+
